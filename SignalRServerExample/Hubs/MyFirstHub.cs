1da5e8f93bef01808376c619fe2f55494636b385
c9013283 <c9013283@arcelik.com>
2023-12-23 14:25:35 +0300

    Add project files.

---
+++ SignalRServerExample/Hubs/MyFirstHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExample.Hubs
+{
+    public class MyFirstHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            // receievedMessage is named on client-side
+            await Clients.All.SendAsync("receivedMessage",message);
+        }
+    }
+}
---
+++ SignalRServerExample/Program.cs
@@ -0,0 +1,41 @@
+using SignalRServerExample.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options => options.AddDefaultPolicy((policy) => 
+                                    policy.AllowAnyMethod()
+                                          .AllowAnyHeader()
+                                          .AllowCredentials()
+                                          .SetIsOriginAllowed(origin => true)
+                                    ));
+// add signalR
+builder.Services.AddSignalR();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors();
+app.UseRouting();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// define hubs subscribtion
+app.UseEndpoints(endpoints =>
+
+{
+    endpoints.MapHub<MyFirstHub>("/myfirsthub");
+});
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
