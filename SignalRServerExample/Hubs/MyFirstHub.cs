60592d6245caa8b19600a6b0872c401d9d76bb17
csametsahin <mcnblogger@gmail.com>
2024-01-15 20:54:11 +0300

    override onconnectedasync and ondisconnectedasync

--- SignalRServerExample/Hubs/MyFirstHub.cs
+++ SignalRServerExample/Hubs/MyFirstHub.cs
@@ -4,10 +4,23 @@ namespace SignalRServerExample.Hubs
 {
     public class MyFirstHub : Hub
     {
+        static List<string> clients = new List<string>();
         public async Task SendMessage(string message)
         {
             // receievedMessage is named on client-side
             await Clients.All.SendAsync("receivedMessage",message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients",clients);
+            await Clients.All.SendAsync("userConnected", Context.ConnectionId);
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userDisconnected", Context.ConnectionId);
         }
     }
 }
