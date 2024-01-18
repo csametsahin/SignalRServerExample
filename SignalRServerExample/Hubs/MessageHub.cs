c6860d0d705dca2b0b8140f3523ffb76ddbbf358
csametsahin <mcnblogger@gmail.com>
2024-01-18 20:58:23 +0300

    add group to signalr method

--- SignalRServerExample/Hubs/MessageHub.cs
+++ SignalRServerExample/Hubs/MessageHub.cs
@@ -9,14 +9,34 @@ namespace SignalRServerExample.Hubs
          * sadece client'a istek atan client
          * All
          * server'a bağlı olan herkes ile
+         * AllExcept
+         * belirtilen clientlar hariç diğerlerine mesaj yolla connectionidye göre filtrelenir
+         * Clients.Client
+         * ile sadece gönderilen connection idye mesaj gönderilir
+         * Group
+         * Belirtilen gruptaki tüm clientlera bildiri gönderilir
+         * önce gruplar oluşturulmalı ardından clientlar grouplara subscribe olmalı
+         * GroupExcept
+         * Belirtilen gruptaki belirtilen tüm clientlara mesaj gönderir
+         * Clients.GroupExcept(groupName,connectionIds)
+         * Groups
+         * Clients.Groups(groupNames)
+         * OthersInGroup
+         * Mesaj gönderen client hariç tüm clientlara mesaj yollar
          * Other 
          * server'a istek atan client dışında tüm clientlera mesaj gönderirr
+         * User ve Users
+         * authenticated kullanıcılara erişmemizi sağlayan method
          * */
-        public async Task SendMessageAsync(string message)
+        public async Task SendMessageAsync(string message,string groupName)
         {
-            await Clients.Others.SendAsync("receivedMessage", message);
+            await Clients.Group(groupName).SendAsync("receivedMessage",message);
+            //await Clients.Others.SendAsync("receivedMessage", message);
         }
-
+        public async Task AddUserToGroup(string connectionId,string groupName)
+        {
+            await Groups.AddToGroupAsync(connectionId, groupName);
+        }
         public override async Task OnConnectedAsync()
         {
             await Clients.Caller.SendAsync("getConnectionId",Context.ConnectionId);
